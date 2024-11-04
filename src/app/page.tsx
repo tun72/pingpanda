d5f9816bd903141a9b93fe2cf13606b7cbe7cbac
Tun72 <tunt72553@gmail.com>
2024-11-04 23:35:35 +0630

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,61 @@
+import Heading from "./components/heading"
+import MaxWidthWrapper from "./components/max-width-wrapper"
+import { Check } from "lucide-react"
+import ShinyButton from "./components/shiny-button"
+function page() {
+  return (
+    <>
+      <section className="relative py-24 sm:py-32 bg-brand-25">
+        <MaxWidthWrapper className="text-center">
+          <div className=" relative mx-auto text-center flex flex-col items-center gap-10">
+            <div>
+              <Heading>
+                <span>Real-Time SaaS Insight,</span>
+                <br />
+                <span className=" relative bg-gradient-to-r from-brand-700 to-brand-800 text-transparent bg-clip-text">
+                  Deliver to your Discord
+                </span>
+              </Heading>
+            </div>
+            <p className="text-base/7  text-gray-600 max-w-prose  text-pretty">
+              PingPanda is the easiest way to monitor your SaaS. Get instant
+              notifications for
+              <span className="font-semibold text-gray-700">
+                {" "}
+                sales, new users, or any other event{" "}
+              </span>
+              sent directly to your Discord.
+            </p>
+
+            <ul className=" space-y-2  text-base/7 text-gray-600 text-left flex flex-col  sm:items-start">
+              {[
+                "Real-time Discord alerts for critical events",
+                "Buy once, use forever",
+                "Track sales, new users, or any other event",
+              ].map((item, index) => (
+                <li key={index} className="flex gap-1.5 items-center">
+                  <Check className="size-5 shrink-0 text-brand-700" />
+                  {item}
+                </li>
+              ))}
+            </ul>
+
+            <div className="w-full max-w-80">
+              <ShinyButton
+                href="/sign-up"
+                className="relative z-10 h-14 w-full text-base shadow-lg transition-shadow duration-300 hover:shadow-xl"
+              >
+                Start For Free Today
+              </ShinyButton>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </>
+  )
+}
+
+export default page
